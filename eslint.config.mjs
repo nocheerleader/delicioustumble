49bd694c0fb0cdb816eaa7bae4139a9f2fe987c4
nocheerleader <ciara.wearen@gmail.com>
2024-12-27 22:32:42 +0000

    added eslint config file

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import { FlatCompat } from '@eslint/eslintrc'
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+})
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  }),
+]
+
+export default eslintConfig
